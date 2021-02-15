09c0a0c5ee797444b3ee2297f0444d8cca317881
Kevin van Sonsbeek <kevin_787@msn.com>
2021-02-15 22:25:51 +0100

    small changes

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -4,24 +4,39 @@ declare(strict_types=1);
 namespace App\DataFixtures;
 
 use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Generator;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class UserFixture extends BaseFixture
+use function Safe\sprintf;
+
+/**
+ * Class UserFixture
+ *
+ * @package App\DataFixtures
+ */
+class UserFixture extends Fixture
 {
     /**
      * @var UserPasswordEncoderInterface
      */
     private UserPasswordEncoderInterface $passwordEncoder;
 
+    /**
+     * @var Generator
+     */
+    private Generator $faker;
+
     public function __construct(UserPasswordEncoderInterface $passwordEncoder)
     {
         $this->passwordEncoder = $passwordEncoder;
+        $this->faker = new Generator();
     }
 
-    public function loadData(ObjectManager $manager)
+    public function load(ObjectManager $manager): void
     {
-        $this->createMany(10, 'main_users', function ($i) {
+        for ($i = 0; $i < 10; $i++) {
             $user = new User();
             $user->setEmail(sprintf('test%d@example.com', $i));
             $user->setFirstName($this->faker->firstName);
@@ -33,8 +48,8 @@ class UserFixture extends BaseFixture
                 )
             );
 
-            return $user;
-        });
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
